867804fc372910cc0108aea8cbbe69b990b7c63d
BiGDoraemon <91402621+monlovemom@users.noreply.github.com>
2025-08-23 10:27:53 +0700

    listview2

--- flutter_mobile2568/first_app/lib/week3.dart
+++ flutter_mobile2568/first_app/lib/week3.dart
@@ -5,19 +5,35 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> listproduct = ['Apple', 'Samsung', 'Oppo'];
     return Scaffold(
       appBar: AppBar(
         title: Text('Color Layout', style: TextStyle(color: Colors.blue)),
         centerTitle: false,
         backgroundColor: Colors.white,
       ),
-      body: ListView(
-        children: [
-          Container(height: 400, width: double.infinity, color: Colors.red),
-          Container(height: 100, width: double.infinity, color: Colors.green),
-          Container(height: 200, width: double.infinity, color: Colors.blue),
-        ],
+      body: ListView.builder(
+        itemCount: listproduct.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Text('$index'),
+            title: Text(listproduct[index]),
+            subtitle: Text('loremlorem...'),
+            trailing: Icon(Icons.edit),
+          );
+        },
       ),
+      // ListView.builder(
+      //   itemCount: listproduct.length,
+      //   itemBuilder: (context, index) {
+      //     return Container(
+      //       height: 200,
+      //       width: double.infinity,
+      //       color: Colors.red,
+      //       child: Text('item ${listproduct[index]}'),
+      //     );
+      //   },
+      // ),
     );
   }
 }
